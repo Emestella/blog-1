32a44419e4606319f68fcc362de039001a76f37f
Emestella <emestellawithadash@gmail.com>
2025-01-19 18:41:08 +0100

    Adding some more stuff for the blog

--- content/posts.ts
+++ content/posts.ts
@@ -1,23 +1,14 @@
-import post1 from '../notion-data/126ce18c-fd83-8023-9ad1-d0e1809b21c3.json'
-import post2 from '../notion-data/127ce18c-fd83-805c-bebd-d6772e18bf02.json'
+import post1 from '../notion-data/180588d3-a5c2-80b2-a63e-c24bd6c8bdb1.json'
 
 const posts = [
   {
-    title: "Maximizing the Benefits of BFF Pattern in API Design",
-    slug: "api-design-in-bff",
+    title: "Il fallait bien recommencer quelque part...",
+    slug: "il-fallait-bien-recommencer-quelque-part",
     content: post1,
-    date: "2023-10-22",
-    description: "Exploring the shift from domain-centric to data-centric API endpoints after introducing BFF. This article introduces BFF and DDD concepts, then proposes API endpoint design strategies in the context of the BFF pattern.",
-    image: undefined
+    date: "2025-01-19",
+    description: "Voil&agrave; que je me remets &agrave; &eacute;crire. Mais, pourquoi donc ? R&eacute;ponse ici.",
+    image: undefined,
   },
-  {
-    title: "Naver Search Bar UX: Analysis and Implementation Guide",
-    slug: "naver-search-bar-ux",
-    content: post2,
-    date: "2023-10-23",
-    description: "Explore the UX analysis of Naver's search bar and strategies for replicating its features. The post examines the UX triggers, selection of search results, and the refreshing of result lists. It also compares coding approaches for implementing search bar functionalities, aiming for a concise and effective execution.",
-    image: undefined
-  }
 ] as Post[];
 
 export default posts;
